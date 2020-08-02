d7e06395226a96198613c2f94d62244a3892ad7e
Andrew Mullan <admullan@gmail.com>
2020-08-02 02:24:55 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,30 @@
 # Write your methods here
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, number)
+  number.times do
+    puts message
+  end
+end
+
+def output_array(array)
+  i = 0
+  while array[i] do
+    puts array[i]
+    i += 1
+  end
+end
+
+def return_string_array(array)
+  i = 0
+  new_array = []
+  while array[i] do
+    new_array[i] = array[i].to_s
+    i += 1
+  end
+  return new_array
+end
